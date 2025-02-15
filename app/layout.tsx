2168bba4ebb1f5e64c0f3d01c5bc630d03e60d45
msaeedsaeedi <msaeedsaeedi@outlook.com>
2025-02-15 12:15:05 +0000

    💄 Updated Font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,16 @@
 import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
-import { Inter } from 'next/font/google';
+import { Poppins } from 'next/font/google';
 import type { ReactNode } from 'react';
 
-const inter = Inter({
-  subsets: ['latin'],
+const poppins = Poppins({
+  weight: ['400', '600', '800'],
+  subsets: ['latin']
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="en" className={poppins.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
