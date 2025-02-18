d439a09f9208d259ab05aae98a58798a4b99d074
msaeedsaeedi <msaeedsaeedi@outlook.com>
2025-02-18 17:45:00 +0000

    ⏪ Font Reverted to `Inter`

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,15 @@
 import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
-import { Poppins } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
 
-const poppins = Poppins({
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
+const inter = Inter({
   subsets: ['latin']
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={poppins.className} suppressHydrationWarning>
+    <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
