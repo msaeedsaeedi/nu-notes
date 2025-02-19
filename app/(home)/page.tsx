959a81580559664ee09272164eeafabc8ba3c8f7
msaeedsaeedi <msaeedsaeedi@outlook.com>
2025-02-19 13:52:43 +0000

    ✨ Default Page for `/notes` added

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,16 +3,16 @@ import Link from 'next/link';
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
+      <h1 className="mb-4 text-2xl font-bold">NU Notes</h1>
       <p className="text-fd-muted-foreground">
         You can open{' '}
         <Link
-          href="/docs"
+          href="/notes"
           className="text-fd-foreground font-semibold underline"
         >
-          /docs
+          /notes
         </Link>{' '}
-        and see the documentation.
+        and see the notes.
       </p>
     </main>
   );
